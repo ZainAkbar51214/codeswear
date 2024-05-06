7bef1d6cf00f2aeef6a8196abf1efd1719027b09
Zain Akbar <zainakbar415@gmai.com>
2024-05-06 15:01:23 +0500

    first commit

---
+++ .history/pages/_app_20240506123413.js
@@ -0,0 +1,12 @@
+import '../src/app/globals.css';
+import RootLayout from '../src/app/layout';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
+}
+
+export default MyApp;
--- .history/src/app/layout_20240506124127.js
+++ .history/src/app/layout_20240506124127.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import Navbar from "./Components/Navbar";
+import Navbar from "../../Components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
